8be28db38744db3b510943fc5f8ac1b4aafb38a2
niftylettuce <niftylettuce@gmail.com>
2018-06-27 04:45:58 -0500

    Downgraded ava so tests pass

--- test/browser.js
+++ test/browser.js
@@ -3,21 +3,23 @@ const _ = require('lodash');
 const jsdom = require('jsdom');
 const jsdomOld = require('jsdom/lib/old-api');
 
+let window;
+
 test.before.cb(t => {
   jsdomOld.env({
     html: '',
     scripts: [require.resolve('../dist/axe.min.js')],
     virtualConsole: new jsdom.VirtualConsole().sendTo(console),
-    done(err, window) {
+    done(err, _window) {
       if (err) return t.end(err);
-      t.context.window = window;
+      window = _window;
       t.end();
     }
   });
 });
 
 test('should create a new Axe instance', t => {
-  const axe = new t.context.window.Axe({ capture: false });
+  const axe = new window.Axe({ capture: false });
   t.true(_.isObject(axe));
   axe.info('hello');
 });
