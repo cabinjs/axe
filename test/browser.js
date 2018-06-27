7f32d505d1678e796c98c4f84c07503174d40618
niftylettuce <niftylettuce@gmail.com>
2018-06-27 04:48:20 -0500

    Revert "Downgraded ava so tests pass"
    This reverts commit 8be28db38744db3b510943fc5f8ac1b4aafb38a2.

--- test/browser.js
+++ test/browser.js
@@ -3,23 +3,21 @@ const _ = require('lodash');
 const jsdom = require('jsdom');
 const jsdomOld = require('jsdom/lib/old-api');
 
-let window;
-
 test.before.cb(t => {
   jsdomOld.env({
     html: '',
     scripts: [require.resolve('../dist/axe.min.js')],
     virtualConsole: new jsdom.VirtualConsole().sendTo(console),
-    done(err, _window) {
+    done(err, window) {
       if (err) return t.end(err);
-      window = _window;
+      t.context.window = window;
       t.end();
     }
   });
 });
 
 test('should create a new Axe instance', t => {
-  const axe = new window.Axe({ capture: false });
+  const axe = new t.context.window.Axe({ capture: false });
   t.true(_.isObject(axe));
   axe.info('hello');
 });
